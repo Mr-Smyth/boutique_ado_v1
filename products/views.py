32941c017e91ff9718beceda7b4744059baacef6
Eamonn Smyth <e.smyth75@gmail.com>
2020-11-26 10:24:02 +0000

    Added products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A View to show all products also will Sort and Search Queries """
+
+    # Get all the products
+    products = Product.objects.all()
+
+    # Put the products into the context
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
