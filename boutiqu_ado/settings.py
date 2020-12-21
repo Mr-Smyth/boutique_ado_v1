ba14db7169878089b9b7befe3d3b818b54abf807
Eamonn Smyth <e.smyth75@gmail.com>
2020-12-21 11:23:31 +0000

    Setup real emails

--- boutiqu_ado/settings.py
+++ boutiqu_ado/settings.py
@@ -245,4 +245,21 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+# FIRST CHECK IF DEVELOPMENT IS IN OS.ENVIRON TO DETERMINE WHICH EMAIL SETUP
+# TO USE
+
+# so if in development - we will log our emails in the console
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+# else we will use our gmail setup and keys in heroku
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
